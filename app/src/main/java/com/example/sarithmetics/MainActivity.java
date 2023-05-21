3732ae340975685d10e48ffd07d2dce2017c3b00
Jethan <120331534+Cqu3nc3@users.noreply.github.com>
2023-05-21 21:38:21 +0800

    disabled darkmode since it ruins the UI, will add dark mode at some point idk. >.<

--- app/src/main/java/com/example/sarithmetics/MainActivity.java
+++ app/src/main/java/com/example/sarithmetics/MainActivity.java
@@ -5,6 +5,7 @@ import static android.text.TextUtils.isEmpty;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -53,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         /*hook*/
         sessionManager = new SessionManager(getApplicationContext());
