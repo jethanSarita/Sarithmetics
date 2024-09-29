ec52d63386714a47c3842aa68ebbdcc5b03c9e76
Jethan <jethanarita@gmail.com>
2024-09-29 18:20:09 +0800

    Qr code generation v0.5
    Only implemented for transaction history not check out

--- app/src/main/java/com/example/sarithmetics/ReceiptActivity.java
+++ app/src/main/java/com/example/sarithmetics/ReceiptActivity.java
@@ -1,6 +1,7 @@
 package com.example.sarithmetics;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkCapabilities;
@@ -26,16 +27,21 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class ReceiptActivity extends AppCompatActivity {
 
     private static final String TAG = "Receipt Activity";
 
     /*Components*/
-    TextView receipt_info, receipt_subtotal, receipt_total, receipt_customer_payment, receipt_customer_change;
+    TextView receipt_info, receipt_subtotal, receipt_total, receipt_customer_payment, receipt_customer_change, receipt_tq;
     Button receipt_btn_ok;
     RecyclerView receipt_rv;
-    ImageView back_btn;
+    ImageView back_btn, qr_code;
 
     /*Firebase*/
     FirebaseDatabaseHelper firebaseDatabaseHelper;
@@ -80,6 +86,7 @@ public class ReceiptActivity extends AppCompatActivity {
 
     /*Previous Data*/
     Bundle bundle;
+    String key;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,10 +105,14 @@ public class ReceiptActivity extends AppCompatActivity {
         receipt_total = findViewById(R.id.receipt_main_total);
         receipt_customer_payment = findViewById(R.id.receipt_main_customer_payment);
         receipt_customer_change = findViewById(R.id.receipt_main_customer_change);
+        receipt_tq = findViewById(R.id.receipt_tq);
 
         /*Buttons*/
         receipt_btn_ok = findViewById(R.id.receipt_main_btn_ok);
         back_btn = findViewById(R.id.toolbar_back);
+
+        /*Qr code*/
+        qr_code = findViewById(R.id.receipt_main_qr_code);
 
         /*List*/
         receipt_rv = findViewById(R.id.receipt_main_rv);
@@ -119,6 +130,7 @@ public class ReceiptActivity extends AppCompatActivity {
 
         /*Previous Data*/
         bundle = getIntent().getExtras();
+        key = bundle.getString("key");
 
         /*Get Current User Info*/
         getCurrentUserInformation();
@@ -157,32 +169,48 @@ public class ReceiptActivity extends AppCompatActivity {
                 Log.e(TAG, "Data cancelled " + error);
             }
         });
-
-        /*firebaseDatabaseHelper.getCurrentUserRef().get().addOnCompleteListener(task -> {
-            if(task.isSuccessful()){
-                Log.d(TAG, "Got User object: " + task.getResult().getValue());
-                cUser = task.getResult().getValue(User.class);
-                if (cUser != null) {
-                    *//*Set refs*//*
-                    history_ref = firebaseDatabaseHelper.getBusinessTransactionHistoryRef(cUser.getBusiness_code());
-                } else {
-                    Log.d(TAG, "User is null");
-                }
-            } else {
-                Log.e(TAG, "Error getting data", task.getException());
-            }
-            *//*Dismiss loading*//*
-            loadingSystem.dismissDialog();
-        });*/
     }
 
     private void populateViewData() {
-        //continue here
+        firebaseDatabaseHelper.getBusinessTransactionHistoryRef(cUser.getBusiness_code()).child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //qr_code
+
+                MyTransaction transaction = snapshot.getValue(MyTransaction.class);
+
+                receipt_tq.setText(transaction.getItem_count() + " Item(s)");
+                receipt_subtotal.setText("₱" + transaction.getSubtotal());
+                receipt_total.setText("₱" + transaction.getSubtotal());
+                receipt_customer_payment.setText("₱" + transaction.getCustomer_payment());
+                receipt_customer_change.setText("₱" + transaction.getCustomer_change());
+                receipt_info.setText(transaction.getTransaction_date() + " " + key);
+
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                String link = "https://sarithmetics-receipt.vercel.app/" + key + "/" + cUser.getBusiness_code();
+
+                try {
+                    BitMatrix bitMatrix = multiFormatWriter.encode(link, BarcodeFormat.QR_CODE, 300, 300);
+
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+
+                    qr_code.setImageBitmap(bitmap);
+                } catch (WriterException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void setUpReceiptList() {
         receipt_rv.setLayoutManager(new LinearLayoutManager(ReceiptActivity.this));
-        receipt_query = firebaseDatabaseHelper.getBusinessTransactionHistoryRef(cUser.getBusiness_code()).child(bundle.getString("key")).child("items");
+        receipt_query = firebaseDatabaseHelper.getBusinessTransactionHistoryRef(cUser.getBusiness_code()).child(key).child("items");
         FirebaseRecyclerOptions<Item> options =
                 new FirebaseRecyclerOptions.Builder<Item>()
                         .setQuery(receipt_query, Item.class)
